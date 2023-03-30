a66de9dceef81b17495bf4f2b3047691b9402e42
Enzo Gennari <enzo.gennari02@gmail.com>
2023-03-30 14:35:01 -0300

    Conclui requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <main>
+        <Header />
+        <SolarSystem />
+      </main>
     );
   }
 }
